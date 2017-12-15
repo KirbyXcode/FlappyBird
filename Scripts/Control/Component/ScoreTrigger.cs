e22ed9d5d59af22aa8b35cf291196d077d923e0b
KirbyXcode <kirby_yu@hotmail.com>
2017-12-15 09:57:37 +0800

    Add files via upload

---
+++ Scripts/Control/Component/ScoreTrigger.cs
@@ -0,0 +1,35 @@
+﻿using PureMVC.Patterns;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ScoreTrigger : MonoBehaviour 
+{
+    private GameDataProxy dataProxy;
+    private bool isStartGame;
+
+    private void Start()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        dataProxy = Facade.Instance.RetrieveProxy(GameDataProxy.NAME) as GameDataProxy;
+    }
+
+    public void StartGame()
+    {
+        isStartGame = true;
+    }
+
+    public void StopGame()
+    {
+        isStartGame = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && isStartGame)
+        {
+            dataProxy.UpdateScore();
+        }
+    }
+}
---
+++ Scripts/Control/Component/TimeController.cs
@@ -0,0 +1,38 @@
+﻿using PureMVC.Patterns;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour 
+{
+    private GameDataProxy dataProxy;
+
+    private void Start()
+    {
+        dataProxy = Facade.Instance.RetrieveProxy(GameDataProxy.NAME) as GameDataProxy;
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine("UpdateTime");
+    }
+
+    public void StopGame()
+    {
+        StopCoroutine("UpdateTime");
+    }
+
+    private IEnumerator UpdateTime()
+    {
+        dataProxy.InitGameData();
+        
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            if (dataProxy != null) 
+            {
+                dataProxy.AddTime();
+            }
+        }
+    }
+}
