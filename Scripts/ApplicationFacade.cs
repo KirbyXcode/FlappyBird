96156631e6bb67e3a743c665b910a0ada5448843
KirbyXcode <kirby_yu@hotmail.com>
2017-12-15 10:12:16 +0800

    Add files via upload

---
+++ Scripts/ApplicationFacade.cs
@@ -0,0 +1,48 @@
+﻿using PureMVC.Patterns;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SUIFW;
+
+public class ApplicationFacade : Facade 
+{
+    private GameObject env;
+
+    public ApplicationFacade()
+    {
+        //RegisterCommand(Define.Reg_StartGameCommand, typeof(StartGameCommand));
+        RegisterCommand(Define.Reg_RestartGameCommand, typeof(RestartGameCommand));
+        RegisterCommand(Define.Reg_EndGameCommand, typeof(EndGameCommand));
+
+        RegisterProxy(new GameDataProxy());
+        RegisterMediator(new GameMediator());
+
+        AddControllerScripts();
+        AddTriggerScripts();
+    }
+
+    private void AddControllerScripts()
+    {
+        GameObject.FindGameObjectWithTag(Define.Player).AddComponent<BirdController>();
+        env = GameObject.Find("Env");
+        UnityHelper.AddChildComponent<LandMovement>(env, "Landing");
+        UnityHelper.AddChildComponent<PipesMovement>(env, "PipesRoot");
+
+        env.AddComponent<TimeController>();
+    }
+
+    private void AddTriggerScripts()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            UnityHelper.AddChildComponent<PipesAndLandTrigger>(env, "Pipe" + i + "_Up");
+            UnityHelper.AddChildComponent<PipesAndLandTrigger>(env, "Pipe" + i + "_Down");
+            UnityHelper.AddChildComponent<PipesAndLandTrigger>(env, "Land" + i);
+        }
+
+        for (int i = 1; i <= 3; i++)
+        {
+            UnityHelper.AddChildComponent<ScoreTrigger>(env, "Pipe" + i + "_Trigger");
+        }
+    }
+}
---
+++ Scripts/Define.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define 
+{
+    public const string Reg_StartGameCommand = "Reg_StartGameCommand";
+    public const string Reg_RestartGameCommand = "Reg_RestartGameCommand";
+    public const string Reg_EndGameCommand = "Reg_EndGameCommand";
+
+    public const string Msg_DisplayGameInfo = "Msg_DisplayGameInfo";
+    public const string Msg_InitGameMediator = "Msg_InitGameMediator";
+
+    public const string Player = "Player";
+}
